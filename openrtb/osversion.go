4be62eb85e9fc103fb56e269addbc0d325e1836f
Tiago Zortea <tiago.zortea@vungle.com>
2016-11-03 13:33:30 -0700

    refactor(openrtb): Fix typo in other comments

--- openrtb/osversion.go
+++ openrtb/osversion.go
@@ -6,7 +6,7 @@ import (
 
 // OsVersion type is used for representing OS versions and comparing them. The version considers
 // only numeric parts to be valid and takes the generic semver like format of
-// MAJOR.MINOR.[BUILD.]PATCH<any-trailing-parts>. It supports a a optional build part which are
+// MAJOR.MINOR.[BUILD.]PATCH<any-trailing-parts>. It supports an optional build part which is
 // considered insignificant. For instance unofficial releases containing "alpha", "beta", "rc".
 // Therefore, "1.1.2" and "1.1.2rc" are considered the same. Also, each part, separated by the '.'
 // character, is optional; the significance is compared sequentially at its 0th index of the
