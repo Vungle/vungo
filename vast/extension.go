9f289b8d851e1abcb3dccf948c8a7d9114e85d2f
darynl <daryn.lam@vungle.com>
2017-08-31 10:29:24 -0700

    Remove Data field in Extension

--- vast/extension.go
+++ vast/extension.go
@@ -3,13 +3,12 @@ package vast
 // Extension type represents an <Extension> element that contains arbitrary XML data provided by the
 // platform to extend the VAST response.
 type Extension struct {
-	Data          []byte          `xml:",innerxml"`
 	Verifications []*Verification `xml:"AdVerifications>Verification,omitempty"`
 }
 
 type Verification struct {
 	Vendor             string              `xml:"vendor,attr"`
-	ViewableImpression *ViewableImpression `xml:"ViewableImpression"`
+	ViewableImpression *ViewableImpression `xml:"ViewableImpression,omitempty"`
 }
 
 type ViewableImpression struct {
