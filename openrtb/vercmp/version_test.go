b64c716e1f2404f3fc7cc9adef9206da3180f45a
Hubert Wong <hubert@hubert.io>
2017-09-12 22:02:55 -0700

    feat(vercmp): add additional comparison functions (#35)
    * feat(vercmp): add additional comparison functions
    * refactor(vercmp): rearrange code a bit for less LOC
    * doc(vercmp): add some documentation

--- openrtb/vercmp/version_test.go
+++ openrtb/vercmp/version_test.go
@@ -1,13 +1,16 @@
 package vercmp
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestVersionIsAbove(t *testing.T) {
+func TestVersion(t *testing.T) {
 	tests := []struct {
-		v1, v2           string
-		isAbove, isBelow bool
+		v1      string
+		v2      string
+		isAbove bool
+		isBelow bool
 	}{
 		// Testing behavior of valid inputs.
 		{"0.0.0", "0.0.0", false, false},
@@ -23,6 +26,17 @@ func TestVersionIsAbove(t *testing.T) {
 		{"9.10.7", "9.10.7", false, false},
 		{"9.10.7", "9.10.7rc", false, false},
 		{"8.2.15030.773", "8.2.15030.772", true, false},
+		{"8", "8", false, false},
+		{"8", "8.0", false, false},
+		{"8", "8.0.0", false, false},
+		{"8.0", "8", false, false},
+		{"8.0", "8.0", false, false},
+		{"8.0", "8.0.0", false, false},
+		{"8.0.0", "8", false, false},
+		{"8.0.0", "8.0", false, false},
+		{"8.0.0", "8.0.0", false, false},
+		{"8.0.0", "8.0.0rc", false, false},
+		{"8.0.0", "8.0.0-rc1", false, false},
 
 		// Testing behavior of invalid inputs.
 		{"", "9.10.7rc", false, true},
@@ -32,17 +46,52 @@ func TestVersionIsAbove(t *testing.T) {
 		{"a", "b", false, false},
 	}
 
-	for i, test := range tests {
-		t.Logf("Testing %d...", i)
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%v and %v", test.v1, test.v2), func(t *testing.T) {
+			v1, v2 := Version(test.v1), Version(test.v2)
+			isEqual := !test.isAbove && !test.isBelow
+			isAboveOrEqual := test.isAbove || isEqual
+			isBelowOrEqual := test.isBelow || isEqual
 
-		v1, v2 := Version(test.v1), Version(test.v2)
+			if v1.IsAbove(v2) != test.isAbove {
+				t.Errorf("Expected %v to be above %v.", v1, v2)
+			}
 
-		if v1.IsAbove(v2) != test.isAbove {
-			t.Errorf("Expects version %s above %s to be %v.", v1, v2, test.isAbove)
-		}
+			if v2.IsBelow(v1) != test.isAbove {
+				t.Errorf("Expected %v to be below %v.", v2, v1)
+			}
 
-		if v2.IsAbove(v1) != test.isBelow {
-			t.Errorf("Expects version %s below %s to be %v.", v2, v1, test.isBelow)
-		}
+			if v1.IsAboveOrEqual(v2) != isAboveOrEqual {
+				t.Errorf("Expected %v to be above or equal to %v.", v1, v2)
+			}
+
+			if v2.IsBelowOrEqual(v1) != isAboveOrEqual {
+				t.Errorf("Expected %v to be below or equal to %v.", v2, v1)
+			}
+
+			if v1.IsBelow(v2) != test.isBelow {
+				t.Errorf("Expected %v to be below %v.", v1, v2)
+			}
+
+			if v2.IsAbove(v1) != test.isBelow {
+				t.Errorf("Expected %v to be above %v.", v2, v1)
+			}
+
+			if v1.IsBelowOrEqual(v2) != isBelowOrEqual {
+				t.Errorf("Expected %v to be below or equal to %v.", v1, v2)
+			}
+
+			if v2.IsAboveOrEqual(v1) != isBelowOrEqual {
+				t.Errorf("Expected %v to be above or equal to %v.", v2, v1)
+			}
+
+			if v1.IsEqual(v2) != isEqual {
+				t.Errorf("Expected %v to be equal to %v.", v1, v2)
+			}
+
+			if v2.IsEqual(v1) != isEqual {
+				t.Errorf("Expected %v to be equal to %v.", v2, v1)
+			}
+		})
 	}
 }
