bf1fd65db559ec635924284ca2f7fc17695f9bb8
darynl <daryn.lam@vungle.com>
2017-12-15 14:58:13 -0800

    remove inline impression check too.

--- vast/inline_test.go
+++ vast/inline_test.go
@@ -18,7 +18,7 @@ var inlineTests = []vasttest.VastTest{
 	vasttest.VastTest{&vast.InLine{}, nil, "inline_valid.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrInlineMissAdTitle, "inline_without_adtitle.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrInlineMissCreatives, "inline_without_creatives.xml"},
-	vasttest.VastTest{&vast.InLine{}, vast.ErrInlineMissImpressions, "inline_without_impressions.xml"},
+	vasttest.VastTest{&vast.InLine{}, nil, "inline_without_impressions.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrAdSystemMissSystem, "inline_without_adsystem.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrCreativeType, "inline_error_creatives.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrImpressionMissUri, "inline_error_impressions.xml"},
