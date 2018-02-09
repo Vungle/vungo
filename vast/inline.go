3fafc1405b3934456cbfa23e4f7e174990312bc5
darynl <daryn.lam@vungle.com>
2018-02-09 11:28:48 -0800

    fix tests

--- vast/inline.go
+++ vast/inline.go
@@ -48,15 +48,23 @@ func (inline *InLine) Validate() error {
 		errors = append(errors, ErrInlineMissImpressions)
 	}
 
-	// We don't want to over validate, as long as the first impression contains a valid tracker
+	// We don't want to over validate, as long as one impression contains a valid tracker
 	// we accept it.
-	if len(inline.Impressions) > 0 {
-		if err := inline.Impressions[0].Validate(); err != nil {
+	var impressionErr []error
+	for i := range inline.Impressions {
+		if err := inline.Impressions[i].Validate(); err != nil {
 			ve, ok := err.(ValidationError)
 			if ok {
-				errors = append(errors, ve.Errs...)
+				impressionErr = append(impressionErr, ve.Errs...)
 			}
+		} else {
+			impressionErr = nil
+			break
 		}
+	}
+
+	if len(impressionErr) > 0 {
+		errors = append(errors, impressionErr...)
 	}
 
 	for _, creative := range inline.Creatives {
