bcd2e78a03265e173e82675b7c8d64c9b0e8dbe5
Daryn L <daryn.lam@vungle.com>
2018-07-23 19:52:55 -0700

    fix(vast): check for adsystem pointer is not nil or return error. (#54)
    * fix(vast): check for adsystem pointer is not nil or return error.

--- vast/inline_test.go
+++ vast/inline_test.go
@@ -16,10 +16,11 @@ func TestInLineMarshalUnmarshal(t *testing.T) {
 
 var inlineTests = []vasttest.VastTest{
 	vasttest.VastTest{&vast.InLine{}, nil, "inline_valid.xml"},
+	vasttest.VastTest{&vast.InLine{}, vast.ErrInlineMissAdSystem, "inline_without_adsystem.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrInlineMissAdTitle, "inline_without_adtitle.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrInlineMissCreatives, "inline_without_creatives.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrInlineMissImpressions, "inline_without_impressions.xml"},
-	vasttest.VastTest{&vast.InLine{}, vast.ErrAdSystemMissSystem, "inline_without_adsystem.xml"},
+	vasttest.VastTest{&vast.InLine{}, vast.ErrAdSystemMissSystem, "inline_adsystem_without_system.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrCreativeType, "inline_error_creatives.xml"},
 	vasttest.VastTest{&vast.InLine{}, nil, "inline_error_impressions.xml"},
 	vasttest.VastTest{&vast.InLine{}, vast.ErrPricingCurrencyFormat, "inline_error_pricing.xml"},
