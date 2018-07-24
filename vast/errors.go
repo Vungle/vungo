8f904eb2c36a084a6eefbdbe02c089397fcb7585
darynl <daryn.lam@vungle.com>
2018-07-23 19:42:28 -0700

    update PR.

--- vast/errors.go
+++ vast/errors.go
@@ -4,95 +4,95 @@ import "errors"
 
 // Please keep the variables alphabetized.
 var (
-	ErrAdSystemMissSystem = errors.New("AdSystem misses system.")
+	ErrAdSystemMissSystem = errors.New("AdSystem misses system")
 
-	ErrAdType = errors.New("Ad should only contain one of Inline and Wrapper.")
+	ErrAdType = errors.New("Ad should only contain one of Inline and Wrapper")
 
-	ErrCompanionAdsMissCompanions = errors.New("CompanionAds miss Companions.")
+	ErrCompanionAdsMissCompanions = errors.New("CompanionAds miss Companions")
 
-	ErrCompanionAdsWrapperMissCompanions = errors.New("CompanionAdsWrapper misses Companions.")
+	ErrCompanionAdsWrapperMissCompanions = errors.New("CompanionAdsWrapper misses Companions")
 
-	ErrCreativeType = errors.New("Creative should contain one type of NonLinearAds, Linear, CompanionAds.")
+	ErrCreativeType = errors.New("Creative should contain one type of NonLinearAds, Linear, CompanionAds")
 
-	ErrCreativeWrapperType = errors.New("CreativeWrapper should contain one type of NonLinearAds, Linear, CompanionAds.")
+	ErrCreativeWrapperType = errors.New("CreativeWrapper should contain one type of NonLinearAds, Linear, CompanionAds")
 
-	ErrDurationEqualZero = errors.New("Duration is less than zero.")
+	ErrDurationEqualZero = errors.New("Duration is less than zero")
 
-	ErrDurationNegative = errors.New("Duration is negative.")
+	ErrDurationNegative = errors.New("Duration is negative")
 
-	ErrIconMissPosition = errors.New("Icon misses position.")
+	ErrIconMissPosition = errors.New("Icon misses position")
 
-	ErrIconMissProgram = errors.New("Icon misses program.")
+	ErrIconMissProgram = errors.New("Icon misses program")
 
-	ErrIconResourcesFormat = errors.New("Icon resources should contain only one of StaticResource, IFrameResource, HtmlResource.")
+	ErrIconResourcesFormat = errors.New("Icon resources should contain only one of StaticResource, IFrameResource, HtmlResource")
 
-	ErrImpressionMissUri = errors.New("Impression misses Uri.")
+	ErrImpressionMissUri = errors.New("Impression misses Uri")
 
-	ErrInlineMissAdSystem = errors.New("Inline misses AdSystem.")
+	ErrInlineMissAdSystem = errors.New("Inline misses AdSystem")
 
-	ErrInlineMissAdTitle = errors.New("Inline misses AdTitle.")
+	ErrInlineMissAdTitle = errors.New("Inline misses AdTitle")
 
-	ErrInlineMissCreatives = errors.New("Inline AD misses Creatives.")
+	ErrInlineMissCreatives = errors.New("Inline AD misses Creatives")
 
-	ErrInlineMissImpressions = errors.New("Inline misses Impressions.")
+	ErrInlineMissImpressions = errors.New("Inline misses Impressions")
 
-	ErrLinearMissMediaFiles = errors.New("MediaFiles are missed.")
+	ErrLinearMissMediaFiles = errors.New("MediaFiles are missed")
 
-	ErrMediaFileBitrateTooHigh = errors.New("The bitrate of the MediaFile is too high.")
+	ErrMediaFileBitrateTooHigh = errors.New("The bitrate of the MediaFile is too high")
 
-	ErrMediaFileBitrateTooLow = errors.New("The bitrate of the MediaFile is too low.")
+	ErrMediaFileBitrateTooLow = errors.New("The bitrate of the MediaFile is too low")
 
-	ErrMediaFileHeightTooHigh = errors.New("The height of the MediaFile is too high.")
+	ErrMediaFileHeightTooHigh = errors.New("The height of the MediaFile is too high")
 
-	ErrMediaFileHeightTooLow = errors.New("The height of the MediaFile is too low.")
+	ErrMediaFileHeightTooLow = errors.New("The height of the MediaFile is too low")
 
-	ErrMediaFileMissDelivery = errors.New("ErrMediaFile miss Delivery.")
+	ErrMediaFileMissDelivery = errors.New("ErrMediaFile miss Delivery")
 
-	ErrMediaFileMissMimeType = errors.New("ErrMediaFile miss MimeType.")
+	ErrMediaFileMissMimeType = errors.New("ErrMediaFile miss MimeType")
 
-	ErrMediaFileMissUri = errors.New("ErrMediaFile miss Uri.")
+	ErrMediaFileMissUri = errors.New("ErrMediaFile miss Uri")
 
-	ErrMediaFileSize = errors.New("The width and height of MediaFile should be greater than zero.")
+	ErrMediaFileSize = errors.New("The width and height of MediaFile should be greater than zero")
 
-	ErrMediaFileUnsupportedMimeType = errors.New("The MimeType in the MediaFile is not supported.")
+	ErrMediaFileUnsupportedMimeType = errors.New("The MimeType in the MediaFile is not supported")
 
-	ErrMediaFileWidthTooHigh = errors.New("The width of the MediaFile is too high.")
+	ErrMediaFileWidthTooHigh = errors.New("The width of the MediaFile is too high")
 
-	ErrMediaFileWidthTooLow = errors.New("The width of the MediaFile is too low.")
+	ErrMediaFileWidthTooLow = errors.New("The width of the MediaFile is too low")
 
-	ErrNonLinearAdsMissNonLinears = errors.New("NonLinearAds miss NonLinears.")
+	ErrNonLinearAdsMissNonLinears = errors.New("NonLinearAds miss NonLinears")
 
-	ErrNonLinearResourceFormat = errors.New("NonLinear resources should contain only one of StaticResource, IFrameResource, HtmlResource.")
+	ErrNonLinearResourceFormat = errors.New("NonLinear resources should contain only one of StaticResource, IFrameResource, HtmlResource")
 
-	ErrOffsetPercentNegative = errors.New("Percent in Offset is neagtive.")
+	ErrOffsetPercentNegative = errors.New("Percent in Offset is neagtive")
 
-	ErrPricingCurrencyFormat = errors.New("Pricing's Currency must comply ISO-4217 3 letter format.")
+	ErrPricingCurrencyFormat = errors.New("Pricing's Currency must comply ISO-4217 3 letter format")
 
-	ErrPricingMissModel = errors.New("Pricing misses Model.")
+	ErrPricingMissModel = errors.New("Pricing misses Model")
 
-	ErrPricingMissPrice = errors.New("Pricing misses Price.")
+	ErrPricingMissPrice = errors.New("Pricing misses Price")
 
-	ErrUnsupportedDeliveryType = errors.New("Delivery does not equal `progressive` and `streaming`.")
+	ErrUnsupportedDeliveryType = errors.New("Delivery does not equal `progressive` and `streaming`")
 
-	ErrUnsupportedEvent = errors.New("Event type is unsupported.")
+	ErrUnsupportedEvent = errors.New("Event type is unsupported")
 
-	ErrUnsupportedMode = errors.New("Mode is not supported.")
+	ErrUnsupportedMode = errors.New("Mode is not supported")
 
-	ErrUnsupportedPriceModelType = errors.New("PriceModelType is unsuppored.")
+	ErrUnsupportedPriceModelType = errors.New("PriceModelType is unsuppored")
 
-	ErrUnsupportedVersion = errors.New("Version is not supported.")
+	ErrUnsupportedVersion = errors.New("Version is not supported")
 
-	ErrVastMissAd = errors.New("Vast misses Ad.")
+	ErrVastMissAd = errors.New("Vast misses Ad")
 
-	ErrVideoClicksMissClickThroughs = errors.New("ClickThroughs are missed.")
+	ErrVideoClicksMissClickThroughs = errors.New("ClickThroughs are missed")
 
-	ErrVideoDurationTooLong = errors.New("Video duration is too long.")
+	ErrVideoDurationTooLong = errors.New("Video duration is too long")
 
-	ErrVideoDurationTooShort = errors.New("Video duration is too short.")
+	ErrVideoDurationTooShort = errors.New("Video duration is too short")
 
-	ErrWrapperMissAdSystem = errors.New("Wrapper misses AdSystem.")
+	ErrWrapperMissAdSystem = errors.New("Wrapper misses AdSystem")
 
-	ErrWrapperMissImpressions = errors.New("Wrapper misses Impressions.")
+	ErrWrapperMissImpressions = errors.New("Wrapper misses Impressions")
 
-	ErrWrapperMissVastAdTagUri = errors.New("Wrapper misses VastAdTagUri.")
+	ErrWrapperMissVastAdTagUri = errors.New("Wrapper misses VastAdTagUri")
 )
