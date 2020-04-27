f88fc1741e611489eb60be60a6053fb62bdc974d
Ray Cai <ray.cai@vungle.com>
2020-04-28 02:21:43 +0800

    fix(openrtb)[PBJ-1389]: Remove useless TestVideoMarshalUnmarshal UT of video

--- openrtb/video_test.go
+++ openrtb/video_test.go
@@ -1,18 +1,11 @@
 package openrtb_test
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/Vungle/vungo/openrtb"
 	"github.com/Vungle/vungo/openrtb/openrtbtest"
 )
-
-var VideoModelType = reflect.TypeOf(openrtb.Video{})
-
-func TestVideoMarshalUnmarshal(t *testing.T) {
-	openrtbtest.VerifyModelAgainstFile(t, "video.json", VideoModelType)
-}
 
 func TestVideo_Fields(t *testing.T) {
 	if err := openrtbtest.VerifyStructFieldNameWithStandardTextFile(
