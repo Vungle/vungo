33f062dfd54415511a9c67daac0abdedc432c07e
Ray Cai <ray.cai@vungle.com>
2020-04-27 17:32:32 +0800

    fix(openrtb)[PBJ-1389]: Correct volume normalization modes type const value

--- openrtb/volumenormalizationmode.go
+++ openrtb/volumenormalizationmode.go
@@ -6,9 +6,9 @@ type VolumeNormalizationMode int
 
 // Possible values from the OpenRTB spec.
 const (
-	VolumeNormalizationModeNone VolumeNormalizationMode = iota + 1
-	VolumeNormalizationModeAverage
-	VolumeNormalizationModePeak
-	VolumeNormalizationModeLoudness
-	VolumeNormalizationModeCustom
+	VolumeNormalizationModeNone     VolumeNormalizationMode = 0
+	VolumeNormalizationModeAverage  VolumeNormalizationMode = 1
+	VolumeNormalizationModePeak     VolumeNormalizationMode = 2
+	VolumeNormalizationModeLoudness VolumeNormalizationMode = 3
+	VolumeNormalizationModeCustom   VolumeNormalizationMode = 4
 )
