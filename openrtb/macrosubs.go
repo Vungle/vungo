ba8b3e84b6507a99087cccf85a33d4d5c5424299
yingchen0706v <49139592+yingchen0706v@users.noreply.github.com>
2020-09-23 10:07:53 +0800

    fix(lurl)[PBJ-2035]: for jaeger loss notification (#91)
    * fix(lurl)[PBJ-2035]: for jaeger loss notification
    
    * add empty line
    
    * add comments
    
    * refacto
    
    * update method name
    
    Co-authored-by: yingchen0706v <yingchen0706v@vungle.com>

--- openrtb/macrosubs.go
+++ openrtb/macrosubs.go
@@ -72,13 +72,21 @@ var findMatchesPool = sync.Pool{
 // MacroSubs assumes that the BidResponse has exactly one seat, which has exactly one bid.
 // If this is not true, it will return empty string and an error.
 func MacroSubs(stringToSub string, seat *SeatBid, bid *Bid, settlement Settlement, reason LossReason) string {
+	var price string
+	// According to OpenRTB spec2.5, Exchange-specific policy may preclude support for loss notices or
+	// the disclosure of winning clearing prices resulting in ${AUCTION_PRICE} macros being removed (i.e.,
+	// replaced with a zero-length string).
+	if settlement.Price() > 0 {
+		price = strconv.FormatFloat(settlement.Price(), 'f', 9, 64)
+	}
+
 	m := map[macro]string{
 		auctionID:       settlement.AuctionID(),
 		auctionBidID:    bid.ID,
 		auctionImpID:    bid.ImpressionID,
 		auctionSeatID:   seat.Seat,
 		auctionAdID:     bid.AdID,
-		auctionPrice:    strconv.FormatFloat(settlement.Price(), 'f', 9, 64),
+		auctionPrice:    price,
 		auctionCurrency: string(settlement.Currency()),
 		auctionLoss:     strconv.Itoa(int(reason)),
 	}
