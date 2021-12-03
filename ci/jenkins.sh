3c7f214bfccbb69069a69a355ee1c17ae5724e17
Vungo CI by Jenkins <platform-bj@vungle.com>
2021-12-03 14:16:29 +0800

    Remove prod/version file. Just bump versio according to the latest.

--- ci/jenkins.sh
+++ ci/jenkins.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-type hub >/dev/null 2>&1 || { echo >&2 "hub is required. See https://hub.github.com/"; exit 1; }
+# type hub >/dev/null 2>&1 || { echo >&2 "hub is required. See https://hub.github.com/"; exit 1; }
 
 # latest_prod_version returns the latest version deployed in production, e.g. v0.9.0.
 function latest_prod_version() {
   git tag --list "v*.*.*" --sort "version:refname" | tail -n 1 | cut -f 2 -d '-'
-}
-
-# app_version returns the version of the main vungo service.
-function app_version() {
-  local image=$(awk '{print $NF}' ci/prod/version)
-  echo ${image}
 }
 
 # minor_version_bump bumps a given app version in the format of "v<major>.<minor>.<patch>" to
@@ -19,42 +13,6 @@ function minor_version_bump() {
   tag=$1
 
   echo ${tag} | awk -F \. '{ print $1"."$2+1".0" }'
-}
-
-# should_adjust_stage_version returns whether the app version should be adjusted based on the state
-# of the development cycle.
-function should_adjust_stage_version() {
-  tag=$1
-
-  # Staging is always one development cycle ahead of production, but depending on when the latest
-  # release is deployed to production, the latest tag can be 1 or 2 minor versions behind. But, we
-  # know for sure that if we have a `release-*` branch at the latest tag + 1 minor, we know stage
-  # needs to be 1 minor version ahead of the latest release.
-  [ -n "$(git branch -r | grep "origin\/$(release_branch_name ${tag})")" ]
-}
-
-# release_branch_name returns the name of release branch with the given app version.
-function release_branch_name() {
-  echo vungo-release-$1
-}
-
-# update_version updates the version for the given project and app version and
-# creates the release branch locally.
-function update_version() {
-  local tag=$1
-  echo "start update version"
-  echo ${tag} > ci/prod/version
-
-  # Create git branch and commit the change
-  # git diff
-  # git checkout -b $(release_branch_name ${tag})
-  git add ci/prod/version
-  echo "after add"
-  git config user.email "platform-bj@vungle.com"
-  git config user.name "Vungo CI by Jenkins"
-  git commit -m "chore(vungo,release): Vungo version bump"
-  git status
-  echo "update version OK"
 }
 
 # push_git_tag pushes the app version to GitHub.
@@ -68,18 +26,14 @@ function push_git_tag() {
   git push -q ${repo} ${tag} || exit 1
 }
 
-function do_release_work() {
-  git log --graph --decorate --oneline -5
-  if [ $(latest_prod_version) != $(app_version) ]; then
-    echo "Latest deployed version is different from the configured version, aborting release process!"
-    exit 0
-  fi
-
+function do_tag_work() {
+  CURRENT_VERSION=$(latest_prod_version)
   NEXT=$(minor_version_bump $(latest_prod_version))
-  update_version ${NEXT}
-  # send_pull_request ${NEXT} git@github.com:Vungle/vungo.git
-  # push_git_tag $(app_version) git@github.com:Vungle/vungo.git || true
-  #https://${GITHUB_AUTH}@github.com/Vungle/vungo.git
+
+  git log --graph --decorate --oneline -5
+  echo "start to tag current version is ${CURRENT_VERSION}"
+  push_git_tag ${NEXT} git@github.com:Vungle/vungo.git || true
+  echo "push tag to remote repo ${NEXT}"
 }
 
-do_release_work
+do_tag_work
