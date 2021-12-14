1fb724e473271edb56ecf945de1a8ac356f98d04
Vungo CI by Jenkins <platform-bj@vungle.com>
2021-12-14 17:18:10 +0800

    Remove Companion in basic

--- vast/vast2/companion_test.go
+++ vast/vast2/companion_test.go
@@ -2,30 +2,30 @@ package vast2_test
 
 import (
 	"fmt"
+	"github.com/Vungle/vungo/vast/vast2"
 	"reflect"
 	"testing"
 
-	vastbasic "github.com/Vungle/vungo/vast/basic"
 	"github.com/Vungle/vungo/vast/vasttest"
 )
 
-var CompanionModelType = reflect.TypeOf(vastbasic.Companion{})
+var CompanionModelType = reflect.TypeOf(vast2.Companion{})
 
 func TestCompanionMarshalUnmarshal(t *testing.T) {
 	vasttest.VerifyModelAgainstFile(t, "Companion", "companion.xml", CompanionModelType)
 }
 
 var companionTests = []vasttest.VastTest{
-	{VastElement: &vastbasic.Companion{}, File: "companion.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_with_staticresource.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_with_iframeresource.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_with_htmlresource.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_error_adparameters.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_error_htmlresource.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_without_resource.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_without_staticresource.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_without_iframeresource.xml"},
-	{VastElement: &vastbasic.Companion{}, File: "companion_without_htmlresource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_with_staticresource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_with_iframeresource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_with_htmlresource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_error_adparameters.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_error_htmlresource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_without_resource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_without_staticresource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_without_iframeresource.xml"},
+	{VastElement: &vast2.Companion{}, File: "companion_without_htmlresource.xml"},
 }
 
 func TestCompanionValidateErrors(t *testing.T) {
