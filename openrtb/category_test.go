1f95239de93d85d94491592803f97a96f6a003fb
joseph.shi <joseph.shi@vungle.com>
2021-12-24 10:38:23 +0800

    update for test cases

---
+++ openrtb/category_test.go
@@ -0,0 +1,53 @@
+package openrtb_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Vungle/vungo/openrtb"
+)
+
+func TestMarshalCategory(t *testing.T) {
+	tests := []struct {
+		desc   string
+		input  string
+		hasErr bool
+		expect openrtb.Category
+	}{
+		{
+			desc:   "happy case 1",
+			input:  string(openrtb.CategoryConvertible),
+			expect: openrtb.CategoryConvertible,
+		},
+		{
+			desc:   "happy case 2",
+			input:  "IAB2-17",
+			expect: openrtb.CategoryPerformanceVehicles,
+		},
+		{
+			desc:   "error case 1",
+			input:  "IAB2-17rrrrr",
+			hasErr: true,
+			expect: openrtb.Category(""),
+		},
+		{
+			desc:   "error case 2",
+			input:  "xxcs",
+			hasErr: true,
+			expect: openrtb.Category(""),
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("Testing TestMarshalCategory case %d...", i), func(t *testing.T) {
+			got, err := openrtb.MarshalCategory(test.input)
+			if (err != nil) != test.hasErr {
+				t.Errorf("TestMarshalCategory(%s) check err incorrect, got:%v, expect:%v", test.desc, err != nil, test.hasErr)
+			}
+
+			if got != test.expect {
+				t.Errorf("TestMarshalCategory(%s) check result incorrect, got:%v, expect:%v", test.desc, got, test.expect)
+			}
+		})
+	}
+}
